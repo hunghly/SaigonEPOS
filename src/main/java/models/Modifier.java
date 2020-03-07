86f53f290a273406a07c55ec009e71074bde7e77
Hung Ly <hunghly@gmail.com>
2020-03-06 23:00:48 -0600

    added all models, need to do controllers and dao

---
+++ src/main/java/models/Modifier.java
@@ -0,0 +1,39 @@
+package models;
+
+public class Modifier {
+    private int id;
+    private String modifer;
+    private double priceMod;
+
+    public Modifier () { };
+
+    public Modifier (int id, String modifer, double priceMod) {
+        this.id = id;
+        this.modifer = modifer;
+        this.priceMod = priceMod;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getModifer() {
+        return this.modifer;
+    }
+
+    public double getPriceMod() {
+        return this.priceMod;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setModifer(String modifer) {
+        this.modifer = modifer;
+    }
+
+    public void setPriceMod(double priceMod) {
+        this.priceMod = priceMod;
+    }
+}
